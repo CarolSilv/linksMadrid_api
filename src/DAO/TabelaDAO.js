696ec9c1996810a2c2dc71f7a394ba97fff6cf57
CarolSilv <csantsilv@gmail.com>
2024-04-21 11:31:38 -0300

    merge

--- src/DAO/TabelaDAO.js
+++ src/DAO/TabelaDAO.js
@@ -19,4 +19,25 @@ async function getServicosAtivos() {
 
 }
 
+async function insertServico(nome, telefone, instagram, bloco_apt, servico_prestado, descricao_servico, whatsapp){
+  const con = pool()
+
+  const idUser = randomUUID()
+  const query = `INSERT INTO public.servicos_madrid(id,nome,telefone,instagram,bloco_apt, servico_prestado,descricao_servico,whatsapp)
+    VALUES($1,$2,$3,$4,$5,$6,$7,$8)`
+    const data = await con.query(query, [idUser, nome, telefone, instagram, bloco_apt, servico_prestado, descricao_servico, whatsapp]);
+    con.end()
+    return data
+}
+
+async function countAcessos(){
+  const con = pool()
+
+  const query = `SELECT * FROM public.servicos_madrid WHERE ativo = true;`
+  const data = await con.query(query)
+  con.end()
+  return data
+}
+
 module.exports = { getServicos,getServicosAtivos }
+
